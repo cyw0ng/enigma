e9f7263f19c7d08da9607cb85d1fb941c0655dc1
cyw0ng <dev.cyw0ng@gmail.com>
2020-06-27 15:00:02 +0000

    [server] add cwelist definition
    fixed https://gitee.com/cyw0ng/enigma/issues/I1LUIC
    
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

---
+++ database/enigma/tables/cwelist.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `cwelist`
+(
+    `id`            int           NOT NULL AUTO_INCREMENT,
+    `cweId`         int unsigned  NOT NULL,
+    `description`   varchar(4096) NOT NULL,
+    `name`          varchar(45)   NOT NULL,
+    `relationships` varchar(1024) NOT NULL,
+    `status`        varchar(45)   NOT NULL,
+    `weaknessabs`   varchar(45)   NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `cweId_UNIQUE` (`cweId`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8

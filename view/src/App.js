a0bfe622246323e6e9da1e25148a81692cdf16a0
cyw0ng <dev.cyw0ng@gmail.com>
2020-06-27 08:48:39 +0000

    [view] adjust theme to more compact
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,68 @@
 import React from 'react';
 import './App.css';
-
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Layout from './containers/layout/Layout'
 
+const darkTheme = createMuiTheme({
+  props: {
+    MuiButton: {
+      size: 'small',
+    },
+    MuiFilledInput: {
+      margin: 'dense',
+    },
+    MuiFormControl: {
+      margin: 'dense',
+    },
+    MuiFormHelperText: {
+      margin: 'dense',
+    },
+    MuiIconButton: {
+      size: 'small',
+    },
+    MuiInputBase: {
+      margin: 'dense',
+    },
+    MuiInputLabel: {
+      margin: 'dense',
+    },
+    MuiListItem: {
+      dense: true,
+    },
+    MuiOutlinedInput: {
+      margin: 'dense',
+    },
+    MuiFab: {
+      size: 'small',
+    },
+    MuiTable: {
+      size: 'small',
+    },
+    MuiTextField: {
+      margin: 'dense',
+    },
+    MuiToolbar: {
+      variant: 'dense',
+    },
+  },
+  overrides: {
+    MuiIconButton: {
+      sizeSmall: {
+        marginLeft: 4,
+        marginRight: 4,
+        padding: 12,
+      },
+    },
+  },
+  palette: {
+    type: 'dark',
+  },
+});
+
 function App() {
-  return <Layout />
+  return  <ThemeProvider theme={darkTheme}>
+    <Layout />
+  </ThemeProvider>
 }
 
 export default App;

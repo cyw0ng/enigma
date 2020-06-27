17c1ce187ca5aaf76cf2406fe9f13300e5a84553
cyw0ng <dev.cyw0ng@gmail.com>
2020-06-27 08:32:50 +0000

    [view] add some basic layout impl.
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

---
+++ view/src/containers/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import TopBar from './TopBar'
+import LeftDrawer from './LeftDrawer'
+
+import './Layout.css'
+
+export default class Layout extends React.Component {
+    render() {
+        return (
+          <div className="cont-layout-root">
+              <TopBar />
+              <LeftDrawer />
+          </div>
+        );
+    }
+}
---
+++ view/src/containers/layout/LeftDrawer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+export default class LeftDrawer extends React.Component {
+    render() {
+        return <div className="cont-leftdrawer-root">
+            <Drawer
+                anchor='left'
+                variant='permanent'
+            >
+              <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+        </div>
+    }
+}

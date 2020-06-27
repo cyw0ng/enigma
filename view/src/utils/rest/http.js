4bf39612ae54fb03a5b8be78060c82cdb02cb643
cyw0ng <dev.cyw0ng@gmail.com>
2020-06-27 14:05:08 +0000

    [view] add private config for rest endpoints
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

---
+++ view/src/utils/rest/http.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import privateConfig from '../../../config.private'
+
+const instance = axios.create({
+    baseURL: privateConfig.restBaseUrl
+});
+
+export default instance

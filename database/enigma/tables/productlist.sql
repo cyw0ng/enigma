4253e279aa4d7e57bbefc26537a7f288bd019917
cyw0ng <dev.cyw0ng@gmail.com>
2020-06-28 14:05:14 +0000

    [database] format ddl code style
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- database/enigma/tables/productlist.sql
+++ database/enigma/tables/productlist.sql
@@ -1,8 +1,10 @@
-CREATE TABLE `productlist` (
-  `id` int NOT NULL,
-  `productName` varchar(225) NOT NULL,
-  `vendorName` varchar(225) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `vendoeName_idx` (`vendorName`),
-  CONSTRAINT `vendorName` FOREIGN KEY (`vendorName`) REFERENCES `vendorlist` (`vendorName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `productlist`
+(
+    `id`          int          NOT NULL,
+    `productName` varchar(225) NOT NULL,
+    `vendorName`  varchar(225) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `vendoeName_idx` (`vendorName`),
+    CONSTRAINT `vendorName` FOREIGN KEY (`vendorName`) REFERENCES `vendorlist` (`vendorName`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
--- database/enigma/tables/vendorlist.sql
+++ database/enigma/tables/vendorlist.sql
@@ -1,6 +1,8 @@
-CREATE TABLE `vendorlist` (
-  `id` int NOT NULL,
-  `vendorName` varchar(225) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_UNIQUE` (`vendorName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `vendorlist`
+(
+    `id`         int          NOT NULL,
+    `vendorName` varchar(225) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name_UNIQUE` (`vendorName`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;

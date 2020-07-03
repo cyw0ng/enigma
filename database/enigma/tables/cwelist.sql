9f9ac79497f7587c641e164e77d29aca5aded602
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-03 15:54:51 +0000

    [database] update name var length to 225
    fixes #7
    
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- database/enigma/tables/cwelist.sql
+++ database/enigma/tables/cwelist.sql
@@ -3,7 +3,7 @@ CREATE TABLE `cwelist`
     `id`            int           NOT NULL AUTO_INCREMENT,
     `cweId`         int unsigned  NOT NULL,
     `description`   varchar(4096) NOT NULL,
-    `name`          varchar(45)   NOT NULL,
+    `name`          varchar(225)   NOT NULL,
     `relationships` varchar(1024) NOT NULL,
     `status`        varchar(45)   NOT NULL,
     `weaknessabs`   varchar(45)   NOT NULL,

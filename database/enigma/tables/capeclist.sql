59842927cf5f99baa892c9b3bf90f59db50f9f19
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-04 11:27:53 +0000

    [database] format schemas
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- database/enigma/tables/capeclist.sql
+++ database/enigma/tables/capeclist.sql
@@ -1,10 +1,12 @@
-CREATE TABLE `capeclist` (
-                             `id` int NOT NULL AUTO_INCREMENT,
-                             `name` varchar(225) NOT NULL,
-                             `prerequisites` varchar(2048) DEFAULT NULL,
-                             `related_weakness` varchar(225) DEFAULT NULL,
-                             `solutions` varchar(2048) DEFAULT NULL,
-                             `summary` varchar(2048) DEFAULT NULL,
-                             PRIMARY KEY (`id`),
-                             UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+CREATE TABLE `capeclist`
+(
+    `id`               int          NOT NULL AUTO_INCREMENT,
+    `name`             varchar(225) NOT NULL,
+    `prerequisites`    varchar(2048) DEFAULT NULL,
+    `related_weakness` varchar(225)  DEFAULT NULL,
+    `solutions`        varchar(2048) DEFAULT NULL,
+    `summary`          varchar(2048) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8

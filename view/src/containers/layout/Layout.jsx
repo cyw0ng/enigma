152e364a662b7324b0c02a4bcbfc3e56c120f750
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-04 11:33:11 +0000

    [view] update TopBar with back-to-frontpage
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- view/src/containers/layout/Layout.jsx
+++ view/src/containers/layout/Layout.jsx
@@ -9,7 +9,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div className="cont-layout-root">
-        <TopBar />
+        <TopBar backToFrontpage={this.props.selectMenuIndex} />
         <div className="cont-layout-mainarea">
           <LeftDrawer
             menuConfig={this.props.menuConfig}
--- view/src/containers/layout/TopBar.jsx
+++ view/src/containers/layout/TopBar.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 
 export default class TopBar extends React.Component {
+  onTitleClickHandler = () => {
+    this.props.backToFrontpage(-1);
+  };
+
   render() {
     return (
       <div className="cont-topbar-root">
         <AppBar position="fixed">
           <Toolbar>
-            <Typography variant="h6">Enigma</Typography>
-            <Button color="inherit">Login</Button>
+            <Typography
+              className="cont-backfront-btn"
+              variant="h6"
+              onClick={this.onTitleClickHandler}
+            >
+              Enigma
+            </Typography>
           </Toolbar>
         </AppBar>
       </div>

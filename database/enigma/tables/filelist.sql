0bafc8c7b0861381471b2088e91d390ec6cd3076
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-04 12:35:17 +0000

    [database] change filelist ddl as a pointer
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- database/enigma/tables/filelist.sql
+++ database/enigma/tables/filelist.sql
@@ -1,7 +1,10 @@
-CREATE TABLE `filelist` (
-                            `id` int NOT NULL AUTO_INCREMENT,
-                            `filename` varchar(225) DEFAULT NULL,
-                            `blob` longblob,
-                            PRIMARY KEY (`id`),
-                            UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+CREATE TABLE `filelist`
+(
+    `id`         int           NOT NULL AUTO_INCREMENT,
+    `filename`   varchar(225)  NOT NULL,
+    `fileurl`    varchar(1024) NOT NULL,
+    `minio_path` varchar(1024) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8

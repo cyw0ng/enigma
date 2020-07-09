9c5e05ab2e818b12fae415dbd6110c2780519db2
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-09 15:42:11 +0000

    [view] update toolbar impl. with btn separated
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- view/src/modules/attackAnalyzer/graphProfiler/components/Toolbar.jsx
+++ view/src/modules/attackAnalyzer/graphProfiler/components/Toolbar.jsx
@@ -5,10 +5,54 @@ import ZoomOutIcon from "@material-ui/icons/ZoomOut";
 import SettingsBackupRestoreIcon from "@material-ui/icons/SettingsBackupRestore";
 
 export default class Toolbar extends React.Component {
-  onZoomInHandler = () => this.props.graphObj.graph.zoomIn();
-  onZoomOutHandler = () => this.props.graphObj.graph.zoomOut();
-  onZoomActualHandler = () => this.props.graphObj.graph.zoomActual();
+  state = {
+    btns: [
+      {
+        id: "zoom-in",
+        icon: <ZoomInIcon fontSize="small" />,
+        onClickHandler: (event) => this.onZoomHandler("in"),
+        disabled: false,
+      },
+      {
+        id: "zoom-out",
+        icon: <ZoomOutIcon fontSize="small" />,
+        onClickHandler: (event) => this.onZoomHandler("out"),
+        disabled: false,
+      },
+      {
+        id: "zoom-actual",
+        icon: <SettingsBackupRestoreIcon fontSize="small" />,
+        onClickHandler: (event) => this.onZoomHandler("actual"),
+        disabled: true,
+      },
+    ],
+  };
 
+  onZoomHandler = (status) => {
+    switch (status) {
+      case "in":
+        this.props.graphObj.graph.zoomIn();
+        break;
+      case "out":
+        this.props.graphObj.graph.zoomOut();
+        break;
+      case "actual":
+        if (this.props.graphObj.graph.view.scale !== 1) {
+          this.props.graphObj.graph.zoomActual();
+        }
+    }
+
+    let zoomActualIndex = this.state.btns.findIndex(
+      (btn) => btn.id === "zoom-actual"
+    );
+    let isCurrentDisabled = this.state.btns[zoomActualIndex].disabled;
+    let updateBtns = Array.from(this.state.btns);
+    let newDisabledValue = this.props.graphObj.graph.view.scale === 1;
+    if (isCurrentDisabled !== newDisabledValue) {
+      updateBtns[zoomActualIndex].disabled = newDisabledValue;
+      this.setState({ btns: updateBtns });
+    }
+  };
   render() {
     if (this.props.graphObj == null) {
       return null;
@@ -18,15 +62,11 @@ export default class Toolbar extends React.Component {
 
     return (
       <div>
-        <IconButton onClick={this.onZoomInHandler}>
-          <ZoomInIcon fontSize="small" />
-        </IconButton>
-        <IconButton onClick={this.onZoomOutHandler}>
-          <ZoomOutIcon fontSize="small" />
-        </IconButton>
-        <IconButton onClick={this.onZoomActualHandler}>
-          <SettingsBackupRestoreIcon fontSize="small" />
-        </IconButton>
+        {this.state.btns.map((btn) => (
+          <IconButton disabled={btn.disabled} onClick={btn.onClickHandler}>
+            {btn.icon}
+          </IconButton>
+        ))}
       </div>
     );
   }

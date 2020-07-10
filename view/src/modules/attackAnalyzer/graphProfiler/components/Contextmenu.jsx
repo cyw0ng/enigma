9549e3c432389a1ea0e711cd4e9dc23950551cc0
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-10 15:46:27 +0000

    [view] contribute to contextmenu, closes #53
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- view/src/modules/attackAnalyzer/graphProfiler/components/Contextmenu.jsx
+++ view/src/modules/attackAnalyzer/graphProfiler/components/Contextmenu.jsx
@@ -5,6 +5,16 @@ import { CSSTransitionGroup } from "react-transition-group";
 import "./Contextmenu.css";
 
 export default class Contextmenu extends React.Component {
+  btns = [
+    {
+      id: "obj-type",
+      style: { fontWeight: "bold" },
+      appearsCb: (popupType) =>
+        ["mask", "vertex", "edge"].indexOf(popupType) > -1,
+      textCb: (popupType) => popupType,
+    },
+  ];
+
   handleOnMaskClick = (evt) => {
     if (evt.target.dataset.role === "mask") {
       this.props.onCloseContextmenu();
@@ -13,11 +23,26 @@ export default class Contextmenu extends React.Component {
 
   onNativeCtxm = (evt) => evt.preventDefault();
 
+  getPopupType = (cell) => {
+    if (cell == null) {
+      return "mask";
+    }
+
+    if (cell.vertex) {
+      return "vertex";
+    }
+
+    if (cell.edge) {
+      return "edge";
+    }
+  };
+
   render() {
     if (this.props.popupProfile == null) {
       return null;
     }
     const popupProfile = this.props.popupProfile;
+    const popupType = this.getPopupType(popupProfile.cell);
     return (
       <div
         className="cont-graphprofiler-ctxm-mask"
@@ -37,9 +62,13 @@ export default class Contextmenu extends React.Component {
               top: popupProfile.graph.lastMouseY,
             }}
           >
-            <Typography>Button1</Typography>
-            <Typography>Button2</Typography>
-            <Typography>Button3</Typography>
+            {this.btns.map((btn) =>
+              btn.appearsCb(popupType) ? (
+                <Typography style={btn.style}>
+                  {btn.textCb(popupType)}
+                </Typography>
+              ) : null
+            )}
           </div>
         </CSSTransitionGroup>
       </div>

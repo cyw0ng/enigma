1f5168f1551a4bcb481d50299ead4ca70032b516
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-11 04:42:02 +0000

    [database] add gprof schemas, fixes #73
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

---
+++ database/enigma/tables/gprof/gprofprojects.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `gprofprojects`
+(
+    `id`        int         NOT NULL,
+    `projectId` varchar(45) NOT NULL,
+    `name`      varchar(45) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8

c65c9265d8b0f53b1fc5f7c449be714b086eb2f8
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-18 11:06:13 +0000

    [view] remove redundant rpanel panels
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- view/src/modules/attackAnalyzer/graphProfiler/components/GraphOps.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class GraphOps extends React.Component {
-  render() {
-    return <div className="cont-graph-ops-root">GraphOps</div>;
-  }
-}
--- view/src/modules/attackAnalyzer/graphProfiler/components/RightPanel.jsx
+++ view/src/modules/attackAnalyzer/graphProfiler/components/RightPanel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ObjectInsight from "./ObjectInsight";
-import GraphOps from "./GraphOps";
 
 import "./RightPanel.css";
 
@@ -8,7 +7,6 @@ export default class RightPanel extends React.Component {
   render() {
     return (
       <div className="cont-gprof-rpanel-root">
-        <GraphOps />
         <ObjectInsight
           mxObjectIdFocused={this.props.mxObjectIdFocused}
           graphObj={this.props.graphObj}

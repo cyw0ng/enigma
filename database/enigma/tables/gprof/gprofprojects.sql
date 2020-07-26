aec7e23eceeb8db01b7f6daa1ee31371dcb32138
cyw0ng <dev.cyw0ng@gmail.com>
2020-07-26 15:53:25 +0000

    [database] update gprofprojects DDL, links #101
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- database/enigma/tables/gprof/gprofprojects.sql
+++ database/enigma/tables/gprof/gprofprojects.sql
@@ -1,8 +1,12 @@
 CREATE TABLE `gprofprojects`
 (
-    `id`        int         NOT NULL,
-    `projectId` varchar(45) NOT NULL,
-    `name`      varchar(45) NOT NULL,
+    `id`            int          NOT NULL,
+    `projectId`     varchar(225) NOT NULL,
+    `name`          varchar(225) NOT NULL,
+    `created`       varchar(45) DEFAULT NULL,
+    `last_modified` varchar(45) DEFAULT NULL,
+    `is_readonly`   tinyint     DEFAULT NULL,
+    `namespace`     varchar(45) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
